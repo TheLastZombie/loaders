da6503e49b4a2a66fc06a1166068aabe212e8760
TLZ <roesch.eric@protonmail.com>
2020-10-18 11:43:54 +0200

    Add IKEA Downloader

--- loaders/Archillect Downloader.js
+++ loaders/Archillect Downloader.js
@@ -11,10 +11,10 @@ console.log('')
 const response = require('../tools/request')('http://archillect.com/')
 
 // Do for each image
-for (let i = 0; i < response.match(/<a class="post" href="\/\d+">/).toString().slice(23, -2); i++) {
-  console.log('Downloading ' + (i + 1) + '/' + response.match(/<a class="post" href="\/\d+">/).toString().slice(23, -2) + ' (' + (i + 1) + ')...')
+for (let i = 0; i < response.match(/<a class="post" href="\/\d+?">/).toString().slice(23, -2); i++) {
+  console.log('Downloading ' + (i + 1) + '/' + response.match(/<a class="post" href="\/\d+?">/).toString().slice(23, -2) + ' (' + (i + 1) + ')...')
 
   // Download image file
   var temp = require('../tools/request')('http://archillect.com/' + (i + 1))
-  require('../tools/download')(temp.match(/<img id="ii" src=".+">/).toString().slice(18, -2))
+  require('../tools/download')(temp.match(/<img id="ii" src=".+?">/).toString().slice(18, -2))
 }
---
+++ loaders/IKEA Downloader.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-eval, no-undef */
+
+// Show header
+console.log('IKEA Downloader')
+console.log('')
+
+// Create directory
+require('../tools/directory')('IKEA kataloger')
+
+// Get catalogue database
+console.log('Retrieving catalogue database...')
+console.log('')
+const response = require('../tools/request')('https://ikeamuseum.com/wp-content/themes/ikea-museum/includes/catalogues/external/listing.json')
+require('fs').writeFileSync('listing.json', response)
+
+eval(response)
+
+// Do for each catalogue
+for (let i = 0; i < listings.length; i++) {
+  console.log('Downloading ' + (i + 1) + '/' + listings.length + ' (' + listings[i].id + ')...')
+
+  // Download catalogue file
+  var temp = require('../tools/request')('https://ikeacatalogues.ikea.com/sv-' + listings[i].id)
+  require('../tools/download')(temp.match(/"downloadPdfUrl":".+?"/).toString().slice(18, -1), listings[i].id + '.pdf')
+}

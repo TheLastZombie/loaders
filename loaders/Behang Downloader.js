0d0ab7c2657f0090901f76c973c9af5aaa9c4660
TLZ <roesch.eric@protonmail.com>
2021-03-01 09:24:31 +0100

    Improve Behang Downloader logging

--- loaders/Behang Downloader.js
+++ loaders/Behang Downloader.js
@@ -7,12 +7,12 @@ require('../tools/directory')('Behang')
 
 // Get wallpaper list
 console.log('Getting wallpaper list...')
-console.log('')
 const response = require('../tools/request')('https://knokfirst.com/behang_manifest.json', true)
 require('fs').writeFileSync('behang_manifest.json', JSON.stringify(response))
 
 // Do for each category
 for (let i = 0; i < response.wallpapers.category.length; i++) {
+  console.log('')
   console.log('Downloading category ' + (i + 1) + '/' + response.wallpapers.category.length + ' (' + response.wallpapers.category[i].name + ')...')
   console.log('')
 
